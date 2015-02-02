779c0bca6dca462b28718f93dcbb4cee0d984667
Lauri Naalisvaara <lauri.naalisvaara@aalto.fi>
2015-02-02 12:46:19 +0200

    mongodb add

---
+++ cookbooks/metadata.rb
@@ -0,0 +1,7 @@
+name        "VideoSync"
+description 'Example cookbook for MongoDB Berkshelf based install'
+maintainer  "Magneetto Media"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends 'mongodb'
